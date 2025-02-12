6c92e124028e4a836eb2c7bc63c780fcdcda4b05
Leonidas MV <yordy.lmv.2000@gmail.com>
2025-02-12 22:37:59 +0100

    last

--- wines/views.py
+++ wines/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Vino
 from django.core.files.storage import default_storage
 
@@ -8,13 +8,13 @@ def add_wine(request):
         nombre = request.POST["nombre"]
         descripcion = request.POST["descripcion"]
         precio = request.POST["precio"]
-        imagen = request.FILES["imagen"]
+        # imagen = request.FILES["imagen"]
 
         vino = Vino(
-            nombre=nombre, descripcion=descripcion, precio=precio, imagen=imagen
+            nombre=nombre, descripcion=descripcion, precio=precio, imagen="test"
         )
         vino.save()
-        return render(request, "wines/collection.html")
+        return redirect("collection")
     return render(request, "wines/add_wine.html")
 
 
@@ -22,8 +22,25 @@ def store(request):
     return render(request, "wines/store.html")
 
 
+# def collection(request):
+#     vinos = Vino.objects.all()
+#     return render(request, "wines/collection.html", {"vinos": vinos})
+
+
 def collection(request):
-    return render(request, "wines/collection.html")
+    vinos = Vino.objects.all()
+    categorias = Vino.objects.values_list(
+        "categoria", flat=True
+    ).distinct()  # Obtener el poder máximo de los vinos
+
+    return render(
+        request,
+        "wines/collection.html",
+        {
+            "vinos": vinos,
+            "categorias": categorias,
+        },
+    )
 
 
 def cata(request):
